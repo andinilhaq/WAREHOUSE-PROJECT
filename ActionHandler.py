ff372e6d4512bb271fe0b109b03f525986939f60
Danil <101421328+andinilhaq@users.noreply.github.com>
2022-06-17 21:28:19 +0700

    Project Final

--- ActionHandler.py
+++ ActionHandler.py
@@ -1,103 +1,156 @@
 import pandas as pd
 
-product={}
-description={}
-stock={}
-money=0
-warehouse={}
-total_cost=0
-flag=0
-cart={}
-
-data = pd.read_csv('store_data.csv')
-df=pd.DataFrame(data)
-
 def ADDITEM():
-    p_no= int(input('Enter item number: '))
-    p_name= input('Enter item description: ')
-    p_stock= int(input('Enter item stock: '))
-    p_price= float(input('Enter item price: '))
-    n=0
-    for i in range(len(warehouse)):
-        if (p_no in warehouse):
-            p_no+=1
-            n=1
-    if n==1:
-        print("Item number already exist, changing value to ",p_no)
+        details= pd.read_csv('store_data.csv')
+        df= pd.DataFrame(details)
+
+        global num
+        global name
+        global price 
+        global stock 
+        num = int(input('Enter item number: '))
+        name = (input('Enter item description: '))
+        check = (len(df[(df.Number == num)]) < 1 and len(df[(df.Name == name) ]) < 1)
+
+        if check and num != "":
+            price = float(input('Enter item price: '))
+            stock = int(input('Enter item stock: '))
+            warehouse = {'Number' : [num], 
+                         'Name' : [name],
+                         'Stock' : [stock],
+                         'Price': [price]}
+            updt=pd.DataFrame(warehouse)
+            updt.to_csv('store_data.csv', mode='a', index = False, header=False)
+            print('Item successfully added!\n')
+            note='success'
+            if note == 'success':
+                lognote = pd.read_csv('ActionLog.csv')
+                df2= pd.DataFrame(lognote)
+                Log = "ADD_ITEM"
+                product = name
+                stock_change= stock
+                act_log={'ActionLog': [Log],
+                         'Name': [product],
+                         'StockChange': [stock_change],
+                         'CurrenStock': [stock]}
+                ActLog=pd.DataFrame(act_log)
+                ActLog.to_csv('ActionLog.csv', mode='a', index = False, header=False)
+        else:
+            print('Product already exist\n')
     
-    warehouse.update({'no':p_no,
-                      'name':p_name,
-                      'stock':p_stock,
-                      'price':p_price})
-
-    print("Item was added successfully!\n")
-    return warehouse
-
 def ADDSTOCK():
-    p_no= int(input('Enter item number: '))
-    if (p_no in warehouse):
-        products=warehouse[p_no]
-        print("Item to be added: ", products['name'] )
-        print("Current item stock: ", products['stock'])
-        qty = int(input("Add item stock: "))
-        products['stock']=products['stock']+qty
+    details= pd.read_csv('store_data.csv')
+    df= pd.DataFrame(details)
+
+    global num
+    num = int(input('Enter item number: '))
+
+    check= (len(df[(df.Number == num)])>0)
+    if check and num !="":
+        print()
+        indeks = df[df['Number']==num].index.item()
+        stock = df['Stock'][indeks] 
+        print('Item to be added: ', df['Name'][indeks])
+        amount= int(input('Enter stock amount: '))
+        stock_updt=stock + amount
+        df['Stock'][indeks]=stock_updt
+        details.to_csv('store_data.csv', mode='r+', index = False)
+        note= 'success'
+        print('Stock updated!')
+        if note == 'success':
+            lognote = pd.read_csv('ActionLog.csv')
+            df2= pd.DataFrame(lognote)
+            Log = "ADD_STOCK"
+            product = df['Name'][indeks]
+            stock_change= amount
+            act_log={'ActionLog': [Log],
+                     'Name': [product],
+                     'StockChange': [stock_change],
+                     'CurrenStock': [stock_updt]}
+            ActLog=pd.DataFrame(act_log)
+            ActLog.to_csv('ActionLog.csv', mode='a', index = False, header=False)
+            return stock_updt
     else:
         print("Item doesn't exist\n")
-    
-    return warehouse
 
-def BUYPRODUCT():
-    p_no= int(input("Enter item number: "))
-    if(p_no in warehouse):
-        print("Item: ", warehouse[p_no]['name'])
-        buy_amount= int(input("Enter purchase amount: "))
-        products = warehouse[p_no]
-        current_stock = products['stock']
-        if(flag==1):
-            flag=0
-        if(buy_amount<=current_stock):
-            current_stock = products['stock']
-            stock_updt = current_stock-buy_amount
-            item_price = buy_amount*products['price']
-            total_cost = total_cost+item_price
-            print(products['name'],"added to cart: ","Rp",item_price)
-            cart.update({p_no:{'name':products['name'],
-                               'amount':buy_amount,
-                               'total':item_price}})
-            products['stock'] = stock_updt
-        else:
-            print("item out of stock!\n")
+def TAKEITEM():
+    details= pd.read_csv('store_data.csv')
+    df= pd.DataFrame(details)
+
+    global num
+    global stock
+    num = int(input('Enter item number: '))
+
+    check= (len(df[(df.Number == num)])>0)
+    if check and num !="":
+        print()
+        indeks = df[df['Number']==num].index.item()
+        stock = df['Stock'][indeks]
+        print('Item to be taken: ', df['Name'][indeks])
+        amount= int(input('Enter take amount: '))
+        stock_updt=stock - amount
+        df['Stock'][indeks]=stock_updt
+        details.to_csv('store_data.csv', mode='r+', index = False)
+        print('Stock updated!')
+        note='success'
+        if note == 'success':
+            lognote = pd.read_csv('ActionLog.csv')
+            df2= pd.DataFrame(lognote)
+            Log = "TAKE_ITEM"
+            product = df['Name'][indeks]
+            stock_change= amount
+            act_log={'ActionLog': [Log],
+                     'Name': [product],
+                     'StockChange': [stock_change],
+                     'CurrenStock': [stock_updt]}
+            ActLog=pd.DataFrame(act_log)
+            ActLog.to_csv('ActionLog.csv', mode='a', index = False, header=False)
+            return stock_updt
     else:
-        print("Sorry! We don't have such an item!\n")
+        print("Item doesn't exist\n")
 
-    return warehouse, cart
-
-def CHECKOUT():
-    print()
-    print("You bought the following items: ",cart)
-    print("Total: Rp ",round(total_cost,2))
-    money=money+total_cost
-    total_cost=0
-    flag=1
-    cart.clear()
-    print()
-    print("You can still purchase items after check out, your cart has been reset. To quit press q")
-    print()
-
-def STATUS():
-    CurrentMoney=money
-    print()
-    print('Current Store Money: Rp ', CurrentMoney)
-    print('Current Store info:')
-    for i in range(0,len(warehouse)):
-        products=warehouse[i+1]
-        print('- Product: ', products['name'], '\t, Price: Rp ', products['price'], '\t, Stock:', products['stock'])
-    print()
-
-def ActionLog():
-    details = pd.read_csv('Warehouse.csv', sep=';', header = None)
-    df=pd.DataFrame(details)
-    warehouse.to_csv('Warehouse.csv','a',index = False, header = None)
-
-database = pd.DataFrame.from_dict(warehouse, orient='index')
-database.to_csv('store_data.csv',mode ='a',index = False, header = None)
+comm='y'
+while (comm!= 'q' or comm!='Q'):
+    print('='*60)
+    print('{:^60}'.format("WAREHOUSE APP"))
+    print('='*60)
+    print('''\nCommand list:
+    1. [N] = Add item
+    2. [A] = Add stock
+    3. [T] = Take item
+    4. [H] = Help (Show command list)
+    5. [S] = Warehouse Status
+    6. [Q] = Quit
+    ''')
+    comm= input("Please enter a command: ")
+    if (comm == 'N' or comm == 'n'):
+        ADDITEM()
+    elif (comm == 'A' or comm == 'a'):
+        ADDSTOCK()
+    elif (comm == 'T' or comm == 't'):
+        TAKEITEM()
+    elif (comm == 'H' or comm == 'h'):
+        print('''\nCommand list:
+    1. [N] = Add item
+    2. [A] = Add stock
+    3. [T] = Take item
+    4. [H] = Help (Show command list)
+    5. [S] = Warehouse Status
+    6. [Q] = Quit
+    ''')
+    elif (comm == 'S' or comm == 's'):
+        details= pd.read_csv('store_data.csv')
+        df= pd.DataFrame(details)
+        print('='*60)
+        print('{:^60}'.format('WAREHOUSE STATUS'))
+        print('='*60)
+        print()
+        print('Current Store info:')
+        print(df)
+        print()
+    elif (comm == 'Q' or comm == 'q'):
+        print()
+        print('Thanks for using WAREHOUSE APP!')
+        break
+    else:
+        print("Unknown input, please enter correct command! [H]: help\n")
